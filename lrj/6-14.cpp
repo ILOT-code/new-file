5ab78a9bec1236ede6a57cd1ea55917b0bb1914b
ILOT-code <a923865873@outlook.com>
2022-03-21 16:29:52 +0800

    22/3/21

--- lrj/6-14.cpp
+++ lrj/6-14.cpp
@@ -1,44 +1,95 @@
-// UVa10305 Ordering Tasks
-// Rujia Liu
-// 题意：输入n和m，以及m个二元组(i,j)，求1~n的一个排列使得对于每个(i,j)，i在j的前面
-// 算法：拓扑排序。注意m可能等于0
-#include<cstdio>
-#include<cstring>
-const int maxn = 1000;
-int n, m, G[maxn][maxn], c[maxn], topo[maxn], t;
-
-bool dfs(int u){
-  c[u] = -1;
-  for(int v = 0; v < n; v++) if(G[u][v]) {
-    if(c[v]<0) return false;
-    else if(!c[v]) dfs(v);
-  }
-  c[u] = 1; topo[--t]=u;
-  return true;
+#include <cstdio>
+#include <cstring>
+#include <queue>
+using namespace std;
+struct Node
+{
+    int r, c, dir;
+    Node(int r=0, int c=0, int dir=0) : r(r), c(c), dir(dir) {}
+};
+const int maxn=10;
+const char *dirs="NESW";
+const char *turns="FLR";
+const int dr[]={-1,0,1,0};
+const int dc[]={0,1,0,-1};
+int r0,c0,dir,r1,c1,r2,c2;
+int has_edge[maxn][maxn][4][3];
+int d[maxn][maxn][4];
+Node pre[maxn][maxn][4];
+int dir_id(char c){
+    return strchr(dirs,c)-dirs;
 }
-
-bool toposort(){
-  t = n;
-  memset(c, 0, sizeof(c));
-  for(int u = 0; u < n; u++) if(!c[u])
-    if(!dfs(u)) return false;
-  return true;
+int turn_id(char c){
+    return strchr(turns,c)-turns;
 }
-
-int main() {
-  while(scanf("%d%d", &n, &m) == 2 && n) {
-    memset(G, 0, sizeof(G));
-    for(int i = 0; i < m; i++) {
-      int u, v;
-      scanf("%d%d", &u, &v); u--; v--;
-      G[u][v] = 1;
+bool inside(Node u){
+    return u.r>=1&&u.r<=9&&u.c>=1&&u.c<=9;
+}
+Node walk(Node u,int turn){
+    int dir=u.dir;
+    if(turn==1) dir=(dir+3)%4;
+    if(turn==2) dir=(dir+1)%4;
+    return Node(u.r+dr[dir],u.c+dc[dir],dir);
+}
+bool read_list(){
+    char s1[99],s2[2];
+    if(scanf("%s%d%d%s%d%d",s1,&r0,&c0,s2,&r2,&c2)!=6) return false;
+    dir=dir_id(s2[0]);
+    r1=r0+dr[dir];
+    c1=c0+dc[dir];
+    memset(has_edge,0,sizeof(has_edge));
+    for(;;){
+        int r,c;
+        scanf("%d",&r);
+        if(r==0) break;
+        scanf("%d",&c);
+        for(;;){
+            scanf("%s",s1);
+            if(s1[0]=='*') break;
+            for(int i=1;i<strlen(s1);i++)
+            has_edge[r][c][dir_id(s1[0])][turn_id(s1[i])]=1;
+        }
     }
-    if(toposort()) {
-      for(int i = 0; i < n-1; i++)
-        printf("%d ", topo[i]+1);
-      printf("%d\n", topo[n-1]+1);
+    return true;
+}
+void print_ans(Node u){
+    vector<Node> nodes;
+    for(;;){
+        nodes.push_back(u);
+        if(u.c==c1&&u.r==r1&&u.dir==dir) break;
+        u=pre[u.r][u.c][u.dir];
     }
-    else
-      printf("No\n"); // 题目没说无解输出什么，应该是保证有解吧
-  }
+    nodes.push_back(Node(r0,c0,dir));
+    int cnt=0;
+    for(int i=nodes.size()-1;i>=0;--i){
+        if(cnt%10==0) printf("    ");
+        printf("  (%d,%d)", nodes[i].r, nodes[i].c);
+        if(++cnt%10==0)   printf("\n");
+    }
+}
+void solve(){
+    memset(d,0,sizeof(d));
+    queue<Node> p;
+    Node u(r1,c1,dir);
+    p.push(u);
+    d[r1][c1][dir]=1;
+    while(!p.empty()){
+        Node u=p.front(); p.pop();
+        if(u.r==r2&&u.c==c2) {print_ans(u);return;}
+        for(int i=0;i<3;++i){
+            Node v=walk(u,i);
+            if(inside(v)&&d[v.r][v.c][v.dir]==0&&has_edge[u.r][u.c][u.dir][i]){
+            p.push(v);
+            d[v.r][v.c][v.dir]=1;
+            pre[v.r][v.c][v.dir]=u;
+            }
+        }
+    }
+    printf("No answer!\n");
+}
+int main(){
+    while(read_list()){
+        solve();
+    }
+    return 0;
 }
---
+++ lrj/6-19.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+using namespace std;
+const int maxn = 52;
+const int maxd = 40000+10;
+int G[maxn][maxn];
+int d[maxn][maxd];
+int vis[maxn];
+int ID(char *s){
+    int n = s[0]-'A';
+    if(s[1]=='+') return 2*n;
+    if(s[1]=='-') return 2*n+1;
+    else         return -1;
+}
+bool dfs(int u){
+    vis[u]=-1;
+    for(int v = 0; v < maxn; ++v)
+    if(G[u][v])
+    if(vis[v] == -1 || (!vis[v] && !dfs(v))) return false;
+    vis[u] = 1;
+    return true;
+}
+int main() {
+    int T;
+    scanf("%d",&T);
+    for(int i = 0; i < T; ++i){
+        char s[4][3];
+        scanf("%s%s%s%s",s[0],s[1],s[2],s[3]);
+        for(int j = 0; j < 4; ++j){
+            int id = ID(s[j]);
+            if(id < 0)     continue;
+            d[id][i]++;
+        }
+    }
+    for(int i = 0; i < maxn; ++i){
+        bool ok=false;
+        for(int j = 0; j < T; ++j)
+            if(d[i][j]) {ok=true;break;}
+        if(!ok)    continue;
+        int couple = 2 * (i/2) + !(i%2);
+        for(int j = 0; j < T; ++j){
+            if(!d[couple][j])    continue;
+            for(int k = 0; k < maxn; ++k)
+                if(d[k][j] && k != couple)
+                    G[i][k]=1;
+                else if(k == couple)
+                    G[i][k]=d[k][j]-1?1:0;
+        }
+    }
+    int ok1=true;
+    for(int i = 0; i < maxn; ++i)
+        if(!vis[i]&&!dfs(i)) {ok1=false;break;}
+    if(!ok1) printf("YES\n");
+    else printf("NO\n");
+    return 0;
+}
