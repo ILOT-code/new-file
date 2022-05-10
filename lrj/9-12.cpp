8e70a388f0e1843098adffde1210509fef54e318
ILOT-code <a923865873@outlook.com>
2022-05-10 21:53:41 +0800

    22/5/10

---
+++ lrj/9-12.cpp
@@ -0,0 +1,33 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+const int maxn = 100000 + 5;
+int n, T;
+vector<int> sons[maxn];
+
+int dp(int u) {
+	if (sons[u].empty()) return 1;
+	int k = sons[u].size();
+	vector<int> d;
+	for (int i = 0; i < k; ++i) d.push_back(dp(sons[u][i]));
+	sort(d.begin(), d.end());
+	int c = (k * T - 1) / 100 + 1;
+	int ans = 0;
+	for (int i = 0; i < c; ++i) ans += d[i];
+	return ans;
+}
+
+int main() {
+	int f;
+	while (~scanf("%d%d", &n, &T) && n) {
+		for (int i = 0; i <= n; i++) sons[i].clear();
+		for (int i = 1; i <= n; i++) {
+			scanf("%d", &f);
+			sons[f].push_back(i);
+		}
+		printf("%d\n", dp(0));
+	}
+	return 0;
+}
---
+++ lrj/9-13.cpp
@@ -0,0 +1,53 @@
+#include<cstdio>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+const int maxn = 200 + 5;
+int n, cnt, d[maxn][2], f[maxn][2];
+vector<int> sons[maxn];
+map<string, int> dict;
+
+int ID(const string &s) {
+	if (!dict.count(s)) dict[s] = cnt++;
+	return dict[s];
+}
+
+int dp(int u, int k) {
+	f[u][k] = 1; d[u][k] = k;
+	for (int i = 0; i < sons[u].size(); ++i) {
+		int v = sons[u][i];
+		if (k == 1) {
+			d[u][k] +=  dp(v, 0);
+			if (!f[v][0]) f[u][k] = 0;
+		} else {
+			d[u][k] += max(dp(v, 1), dp(v, 0));
+			if (d[v][1] == d[v][0]) f[u][k] = 0;
+			if (d[v][0] > d[v][1] && !f[v][0]) f[u][k] = 0;
+			if (d[v][1] > d[v][0] && !f[v][1]) f[u][k] = 0;
+		}
+	}
+	return d[u][k];
+}
+
+int main() {
+	string s1, s2;
+	while ((cin >> n >> s1) && n) {
+		cnt = 0; dict.clear();
+		for (int i = 0; i < n; ++i) sons[i].clear();
+		ID(s1);
+		for (int i = 0; i < n - 1; ++i) {
+			cin >> s1 >> s2;
+			sons[ID(s2)].push_back(ID(s1));
+		}
+		printf("%d ", max(dp(0, 0), dp(0, 1)));
+		bool unique = false;
+		if(d[0][0] > d[0][1] && f[0][0]) unique = true;
+		if(d[0][1] > d[0][0] && f[0][1]) unique = true;
+		if(unique) printf("Yes\n"); else printf("No\n");
+	}
+	return 0;
+}
