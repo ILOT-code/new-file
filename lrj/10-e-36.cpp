6a7c7c41dbc8d0919174c5aadf661f58115c041c
ILOT-code <a923865873@outlook.com>
2022-07-19 23:12:40 +0800

    22/7/19

--- lrj/10-e-36.cpp
+++ lrj/10-e-36.cpp
@@ -1,42 +1,38 @@
 #include<cstdio>
 #include<cstring>
-#include<iostream>
 #include<algorithm>
 using namespace std;
 typedef long long LL;
 
-LL n, f[101], num, ans;
-int a[101], lim[101];
-
-void prework() {
+LL n, f[101], ans;
+void init() {
 	f[1] = 1; f[2] = 1;
 	for (int i = 3; i <= 90; i++) f[i] = f[i - 1] + f[i - 2];
 }
 
+int ori[101], rev[101];
 void print(LL sum) {
 	int cnt = 0;
 	for (int i = 90; i >= 2; i--) {
-		if (sum >= f[i]) a[++cnt] = 1, sum -= f[i];
-		else a[++cnt] = 0;
+		if (sum >= f[i]) ori[++cnt] = 1, sum -= f[i];
+		else ori[++cnt] = 0;
 	}
-	for (int i = 1; i <= cnt; i++) lim[i] = a[90 - i];
+	for (int i = 1; i <= cnt; i++) rev[i] = ori[90 - i];
 }
 
-
-
-LL dp[91][2][2];
-
+LL dp[91][2][2], kinds[91][2][2];
 LL dfs(int digit, int last, bool equal) {
-	LL &res = dp[digit][last][equal];
+	LL &res = dp[digit][last][equal], &kind = kinds[digit][last][equal];
 	if (res != -1) return dp[digit][last][equal];
-	if (!digit) return res = 0;
+	if (!digit) {kind = 1; return res = 0;}
 	int maxd = 1;
-	if (equal) maxd = lim[digit];
-	res = 0;
+	if (equal) maxd = rev[digit];
+	res = kind = 0;
 	for (int i = 0; i <= maxd; ++i) {
 		if (last == 1 && i == 1) continue;
-		bool tem = equal && (i == lim[digit]);
-		res += dfs(digit - 1, i, tem) + i;
+		bool tem = equal && (i == rev[digit]);
+		res += dfs(digit - 1, i, tem) + (i ? kinds[digit - 1][i][tem] : 0);
+		kind += kinds[digit - 1][i][tem];
 	}
 	return res;
 }
@@ -44,19 +40,17 @@ LL dfs(int digit, int last, bool equal) {
 int main() {
 	while (~scanf("%lld", &n)) {
 		memset(dp, -1, sizeof(dp));
-		prework();
-		if (n == 0) { puts("0"); continue; }
+		init();
 		int w = 1;
 		while (n > w * f[w]) {n -= w * f[w], ++ w;}
-		num = f[w + 1];
-		num += n / w - 1;
-		int ws = w - !(n / w), wws = w + (n / w == f[w]);
+		LL num = f[w + 1] + n / w - 1;
+		int wn = w - !(n / w), wl = w + (n / w == f[w]);
 		n %= w;
 		print(num);
-		ans = dfs(ws, 0, 1);
+		ans = dfs(wn, 0, 1);
 		print(num + 1);
-		for (int i = wws; i >= wws - n + 1; i--) ans += (lim[i] == 1);
-		cout << ans << endl;
+		for (int i = wl; i >= wl - n + 1; i--) ans += rev[i];
+		printf("%lld\n", ans);
 	}
 	return 0;
 }
